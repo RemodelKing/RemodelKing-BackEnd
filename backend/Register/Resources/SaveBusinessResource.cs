e84ae16e3e6dcff80a8f04c1c2d626d1197de8c0
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2022-06-06 09:12:47 -0500

    feat: Added business and clients resources

---
+++ backend/Register/Resources/SaveBusinessResource.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Register.Resources;
+
+public class SaveBusinessResource: SaveUserResource
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    [Required]
+    public long Phone { get; set; }
+}
---
+++ backend/Register/Resources/SaveUserResource.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Register.Resources;
+
+public class SaveUserResource
+{
+    [Required]
+    [MaxLength(50)]
+    public string Email { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Password { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string ConfirmPassword { get; set; }
+}
