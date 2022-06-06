d9337c08ea523c3da9dd6457e4fbb7f6d4820869
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2022-06-06 09:11:59 -0500

    feat: Added business and clients mapping

---
+++ backend/Register/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using backend.Register.Domain.Models;
+using backend.Register.Resources;
+
+namespace backend.Register.Mapping;
+
+public class ModelToResourceProfile: Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Client, ClientResource>();
+        CreateMap<Business, BusinessResource>();
+    }
+}
---
+++ backend/Register/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using backend.Register.Domain.Models;
+using backend.Register.Resources;
+
+namespace backend.Register.Mapping;
+
+public class ResourceToModelProfile: Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveClientResource, Client>();
+        CreateMap<SaveBusinessResource, Business>();
+    }
+}
