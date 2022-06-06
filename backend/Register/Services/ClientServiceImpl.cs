06d4728f58b506a24c30f66e06332492160c2407
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2022-06-06 09:13:11 -0500

    feat: Added business and clients services

---
+++ backend/Register/Services/ClientServiceImpl.cs
@@ -0,0 +1,43 @@
+﻿using backend.Register.Domain.Models;
+using backend.Register.Domain.Repositories;
+using backend.Register.Domain.Services;
+using backend.Register.Domain.Services.Communication;
+
+namespace backend.Register.Services;
+
+public class ClientServiceImpl: IClientService
+{
+    private readonly IClientRepository _clientRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ClientServiceImpl(IClientRepository clientRepository, IUnitOfWork unitOfWork)
+    {
+        _clientRepository = clientRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IEnumerable<Client>> ListAsync()
+    {
+        return await _clientRepository.ListAsync();
+    }
+
+    public async Task<ClientResponse> CreateAsync(Client client)
+    {
+        try
+        {
+            //Validations
+            if (client.Password != client.ConfirmPassword)
+                return new ClientResponse("Different passwords was received");
+
+            //Actions
+            await _clientRepository.AddAsync(client);
+            await _unitOfWork.CompleteAsync();
+            return new ClientResponse(client);
+        }
+        catch (Exception e)
+        {
+            return new ClientResponse($"Failed to register a client: {e.Message}");
+        }
+    }
+    
+}
