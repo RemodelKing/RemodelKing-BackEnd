c9ff1fc2ce3558c00803d2e89a293cc100f1967c
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2022-06-06 09:12:25 -0500

    feat: Added business and clients persistence

---
+++ backend/Register/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using backend.Register.Persistence.Context;
+
+namespace backend.Register.Persistence.Repositories;
+
+public class BaseRepository
+{
+    protected readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+}
